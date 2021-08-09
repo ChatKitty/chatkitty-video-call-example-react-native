5a08f7477a30fb3f5ee943a106a479c855f066b4
Aaron Nwabuoku <aaron@chatkitty.com>
2021-08-09 17:19:26 -0400

    Initial commit

---
+++ src/chatkitty/index.ts
@@ -0,0 +1,9 @@
+import ChatKitty from 'chatkitty';
+
+const kitty = new ChatKitty({
+  host: 'localhost:9001/chat',
+  isSecure: false,
+  apiKey: 'b464c6c5-1807-4bc6-b6a4-be04399d8407',
+});
+
+export default kitty;
